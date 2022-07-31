176b38eb64782197b8d4e7758cc82b8de6523341
Prince Nsidibe <prince.nsidibe@globalaccelerex.com>
2022-07-31 18:30:57 +0100

    Change permission to pre-commit hook

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 resource "aws_vpc" "vpc" {
-    enable_dns_hostnames = true
-    enable_dns_support = true
-    cidr_block =var.vpc_cidr_block
-    tags = merge(local.tags, {
-        Name = var.resource_identifier
-    })
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  cidr_block           = var.vpc_cidr_block
+  tags = merge(local.tags, {
+    Name = var.resource_identifier
+  })
 }
 
 resource "aws_internet_gateway" "internet_gateway" {
-    vpc_id = aws_vpc.vpc.id
-    tags = merge(local.tags, {
-        Name = var.resource_identifier
-    })
+  vpc_id = aws_vpc.vpc.id
+  tags = merge(local.tags, {
+    Name = var.resource_identifier
+  })
 }
 
 resource "aws_route_table" "public_route_table" {
